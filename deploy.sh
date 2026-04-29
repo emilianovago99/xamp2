526b5444455fb361ec3b95821c853177edd02337
emilianovago99 <emilianovaldezgomez99@gmail.com>
2026-04-29 12:27:27 -0600

    Add deployment script and Docker Compose configuration

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Bajamos repositorio de GitHub'
+# Copiamos código actualizado de GitHub
+git pull origin main
+
+echo 'Detenemos contenedores de la Aplicación'
+# Detenemos Contenedores
+docker-compose -f docker-compose.prod.yml down
+
+echo 'Inicializamos contenedores y reconstruimos'
+# Inicializamos Contenedores y reconstruimos
+docker-compose -f docker-compose.prod.yml up -d --build
+
+echo 'Actualización finalizada'
